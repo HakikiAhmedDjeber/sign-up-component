88328fb478784192318ca9b886fd40944f24ed0f
Hakiki Ahmed Djeber <Hakikiahmeddjeber@gmail.com>
2023-03-12 22:02:34 +0100

    fix some accessibilty issues

--- Script/main.js
+++ Script/main.js
@@ -1,45 +1,44 @@
 // call all inputs
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const submit = document.querySelector("input[type='submit']");
-// the list of input fileds
-const fileds = document.querySelectorAll("input:not([type='submit'])");
+const firstName = document.getElementById("firstName"),
+  lastName = document.getElementById("lastName"),
+  email = document.getElementById("email"),
+  password = document.getElementById("password"),
+  submit = document.querySelector("input[type='submit']"),
+  // the list of input fileds
+  fileds = document.querySelectorAll("input:not([type='submit'])");
 let check = 0;
 
 // decalre the regEx pattrens
-const namesEx = /[a-z]{1,}/i;
-const emailEx = /\w@\w.\w/i;
+const namesEx = /[a-z]{1,}/i,
+  emailEx = /\w@\w.\w/i;
 
 // check fileds function
 let checkFields = () => {
-  if (!namesEx.test(firstName.value)) {
-    activeInvaldeMode(firstName);
-    check++;
-  } else removeInvaldeMode(firstName);
-  if (!namesEx.test(lastName.value)) {
-    activeInvaldeMode(lastName);
-    check++;
-  } else removeInvaldeMode(lastName);
-  if (!emailEx.test(email.value)) {
-    activeInvaldeMode(email);
-    check++;
-  } else removeInvaldeMode(email);
-  if (password.value == "") {
-    activeInvaldeMode(password);
-    check++;
-  } else removeInvaldeMode(password);
-  if (check != 0) submit.setAttribute("disabled", true);
-  else submit.removeAttribute("disabled");
-  check = 0;
-};
-
-// invalde mode function
-let activeInvaldeMode = (field) => {
-  field.classList.add("invalid-mode");
-  field.nextElementSibling.classList.add("active");
-};
+    if (!namesEx.test(firstName.value)) {
+      activeInvaldeMode(firstName);
+      check++;
+    } else removeInvaldeMode(firstName);
+    if (!namesEx.test(lastName.value)) {
+      activeInvaldeMode(lastName);
+      check++;
+    } else removeInvaldeMode(lastName);
+    if (!emailEx.test(email.value)) {
+      activeInvaldeMode(email);
+      check++;
+    } else removeInvaldeMode(email);
+    if (password.value == "") {
+      activeInvaldeMode(password);
+      check++;
+    } else removeInvaldeMode(password);
+    if (check != 0) submit.setAttribute("disabled", true);
+    else submit.removeAttribute("disabled");
+    check = 0;
+  },
+  // invalde mode function
+  activeInvaldeMode = (field) => {
+    field.classList.add("invalid-mode");
+    field.nextElementSibling.classList.add("active");
+  };
 
 // desactive invalid mode function
 function removeInvaldeMode(field) {
