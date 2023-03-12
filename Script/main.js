e87b151e2012cf74c133b1195627717a11e162c8
Hakiki Ahmed Djeber <Hakikiahmeddjeber@gmail.com>
2023-03-12 15:57:56 +0100

    finish the code

---
+++ Script/main.js
@@ -0,0 +1,56 @@
+// call all inputs
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submit = document.querySelector("input[type='submit']");
+const fileds = document.querySelectorAll("input:not([type='submit'])");
+let check = 0;
+// decalre the regEx pattrens
+const namesEx = /[a-z]{1,}/i;
+const emailEx = /\w@\w.\w/i;
+
+// check fileds function
+let checkFields = () => {
+  if (!namesEx.test(firstName.value)) {
+    activeInvaldeMode(firstName);
+    check++;
+  } else removeInvaldeMode(firstName);
+  if (!namesEx.test(lastName.value)) {
+    activeInvaldeMode(lastName);
+    check++;
+  } else removeInvaldeMode(lastName);
+  if (!emailEx.test(email.value)) {
+    activeInvaldeMode(email);
+    check++;
+  } else removeInvaldeMode(email);
+  if (password.value == "") {
+    activeInvaldeMode(password);
+    check++;
+  } else removeInvaldeMode(password);
+  if (check != 0) submit.setAttribute("disabled", true);
+  else submit.removeAttribute("disabled");
+  check = 0;
+};
+
+// invalde mode function
+let activeInvaldeMode = (field) => {
+  field.classList.add("invalid-mode");
+  field.nextElementSibling.classList.add("active");
+};
+
+// desactive invalid mode function
+function removeInvaldeMode(field) {
+  field.classList.remove("invalid-mode");
+  field.nextElementSibling.classList.remove("active");
+  submit.removeAttribute("disabled");
+  console.log("remove done from " + field.name);
+}
+
+// do the check when we click on submit input
+submit.addEventListener("click", checkFields);
+
+// remove invalid message when we focus on filed
+fileds.forEach((element) => {
+  element.addEventListener("focus", () => removeInvaldeMode(element));
+});
